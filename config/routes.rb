fddbc900c25a09e7851b0082b88b10a61d2eefba
Matt Herrero <mherrero@stanford.edu>
2015-11-19 22:16:25 -0800

    Sign Up Stubs
    Some first steps towards signup page stubs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
-  root 'application#hello'
-  get 'application/hello'
+
+  root 'welcome#index'
+  get 'signup'  => 'users#new'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
